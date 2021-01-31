54c383c76007e34ce008e63ef790a5825064e5c2
junedchhipa <juned.chhipa@gmail.com>
2021-01-31 13:18:11 +0530

    remove apexcharts import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable */
 import Vue3Apexcharts from "./vue3-apexcharts";
-import ApexCharts from 'apexcharts/dist/apexcharts.min';
 
 const install = app => {
   app.component(Vue3Apexcharts.name, Vue3Apexcharts);
-  app.config.globalProperties.$apexcharts = ApexCharts;
 };
 
 Vue3Apexcharts.install = install;
